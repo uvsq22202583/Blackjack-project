0c8e91aa36597e1fe5eda1e752494fe7eaf2fd38
uvsq22202583 <113386267+uvsq22202583@users.noreply.github.com>
2023-03-20 19:55:03 +0100

    Add files via upload

--- blackjack sans interface.py
+++ blackjack sans interface.py
@@ -2,6 +2,7 @@ import random
 import tkinter as tk
 nombre_joueur = 1
 carte_tirée = []
+liste_carte=[]
 nombre_tiré =[]
 croupier = 0
 joueur_1 = 0
@@ -11,16 +12,26 @@ nom_du_joueur=str(input("Quel est votre nom ?"))
 def tirer_une_carte_aléatoire ():
     nombre_ale = random.randint(1,13)
     couleur_ale = random.randint(1,4)
+    if couleur_ale == 1 :
+        couleur_ale = " de coeur"
+    elif couleur_ale == 2 :
+        couleur_ale = " de pique"
+    elif couleur_ale == 3 :
+        couleur_ale = " de carreau"
+    elif couleur_ale == 4 :
+        couleur_ale = " de trèfle"
     carte = str(nombre_ale)+str(couleur_ale)
     if carte not in carte_tirée:
         if  nombre_ale > 1 and nombre_ale < 10 :
             carte_tirée.append(carte)
             nombre_tiré.append(nombre_ale)
         elif nombre_ale >= 10:
-            carte_tirée.append(carte)
             nombre_ale = 10
             nombre_tiré.append(nombre_ale)
+            carte = str(nombre_ale)+str(couleur_ale)
+            carte_tirée.append(carte)
         elif nombre_ale == 1 :
+            carte = "as" +str(couleur_ale)
             carte_tirée.append(carte)
             as_onze= str(input("Voulez-vous que ce soit un as ou un onze?"))
             if as_onze == "as" :
@@ -29,6 +40,7 @@ def tirer_une_carte_aléatoire ():
             elif as_onze == "onze" :
                 nombre_ale = 11
                 nombre_tiré.append(nombre_ale)
+
             
     
 for i in range (4):
@@ -36,8 +48,8 @@ for i in range (4):
     
 croupier = nombre_tiré[0] + nombre_tiré[2]
 joueur_1 = nombre_tiré[1] + nombre_tiré[3]
-print("le croupier a pioché un",nombre_tiré[0])   
-print(nom_du_joueur,"a pioché un",nombre_tiré[1],"et un",nombre_tiré[3],",vous avez donc un score de", joueur_1)   
+print("le croupier a pioché un",carte_tirée[0])   
+print(nom_du_joueur,"a pioché un",carte_tirée[1],"et un",carte_tirée[3],",vous avez donc un score de", joueur_1)   
 
 while joueur_1 < 21:
     demande = str(input("Voulez-vous tirez une carte?"))
@@ -46,19 +58,19 @@ while joueur_1 < 21:
     else :
         tirer_une_carte_aléatoire()
         joueur_1 = joueur_1 + nombre_tiré[-1]
-        print(nom_du_joueur,"vous avez pioché un ",nombre_tiré[-1],"votre score est donc de:", joueur_1)
+        print(nom_du_joueur,"vous avez pioché un ", carte_tirée[-1],"votre score est donc de:", joueur_1)
         
     
 if joueur_1 > 21 :
-    print(nom_du_joueur, "vous avez gagné")
+    print(nom_du_joueur, "vous avez perdu")
 elif joueur_1 == 21 : 
     print(nom_du_joueur, "vous avez gagné")
 else :
-    print("Le coupier avais pioché un ", nombre_tiré[0], "et un ", nombre_tiré[2])
+    print("Le coupier avais pioché un ", carte_tirée[0], "et un ", carte_tirée[2])
     while croupier < 17 :
         tirer_une_carte_aléatoire()
         croupier = croupier + nombre_tiré[-1]
-        print("croupier vous avez pioché un ",nombre_tiré[-1],"votre score est donc de:", croupier)
+        print("croupier vous avez pioché un ",carte_tirée[-1],"votre score est donc de:", croupier)
     while croupier < 21:
         demande = str(input("Croupier voulez-vous tirez une carte?"))
         if demande == "non" :
@@ -66,7 +78,7 @@ else :
         else :
             tirer_une_carte_aléatoire()
             croupier = croupier + nombre_tiré[-1]
-            print("Croupier vous avez pioché un ",nombre_tiré[-1],"votre score est donc de:", croupier)
+            print("Croupier vous avez pioché un ",carte_tirée[-1],"votre score est donc de:", croupier)
             
     if croupier > 21 :
         print("Croupier vous avez", croupier, "vous avez donc perdu,", nom_du_joueur,"vous avez gagné")
